daa3b005877b13f14051608a0f0d41202019179a
andhedrew <theandhedrew@gmail.com>
2023-02-12 15:46:35 -0600

    Spitter Plant mechanically works

--- Actors/Enemies/SpitterPlant/SpitterPlant.gd
+++ Actors/Enemies/SpitterPlant/SpitterPlant.gd
@@ -1,22 +1,24 @@
 extends BaseEnemy
 
 export(PackedScene) var bullet_scene
-export(float) var attack_delay_in_seconds := 50
+export(float) var attack_delay := 50
 onready var bullet_spawn := $BulletSpawn
 onready var player_sensor := $PlayerSensor
 
 func _ready():
 	state = Enums.State.IDLE
 	facing = Enums.Facing.RIGHT
+
+
 func _physics_process(delta):
 	state_timer()
 	switch_state()
 	if invulnerable and $InvulnerableTimer.is_stopped():
 		invulnerable = false
 	
-#	match scale.x:
-#		1.0: facing = Enums.Facing.RIGHT
-#		-1.0: facing = Enums.Facing.LEFT
+	match scale.x:
+		1.0: facing = Enums.Facing.RIGHT
+		-1.0: facing = Enums.Facing.LEFT
 
 
 func apply_gravity():
@@ -39,29 +41,30 @@ func  jump_state():
 
 
 func  attack_state():
-	var bullet = bullet_scene.instance()
-	var xspeed := 0.0
-	var yspeed := 0.0
-	
-	bullet.position = bullet_spawn.global_position
-	bullet.set_collision_mask(2)
-	if facing == Enums.Facing.LEFT:
-		xspeed = bullet.speed * -transform.x.x
-		bullet.rotation_degrees = 180
-	elif facing == Enums.Facing.RIGHT:
-		xspeed = bullet.speed * transform.x.x
-		bullet.rotation_degrees = 0
-	elif facing == Enums.Facing.UP:
-		yspeed = -bullet.speed
-		bullet.rotation_degrees = 270
-	elif facing == Enums.Facing.DOWN:
-		yspeed = bullet.speed
-		bullet.rotation_degrees = 90
-	
-	bullet.velocity = Vector2(xspeed, yspeed)
-	get_parent().get_parent().add_child(bullet)
-	
-	state = Enums.State.IDLE
+	if state_timer > attack_delay:
+		var bullet = bullet_scene.instance()
+		var xspeed := 0.0
+		var yspeed := 0.0
+		
+		bullet.position = bullet_spawn.global_position
+		bullet.set_collision_mask(2)
+		if facing == Enums.Facing.LEFT:
+			xspeed = bullet.speed * -transform.x.x
+			bullet.rotation_degrees = 180
+		elif facing == Enums.Facing.RIGHT:
+			xspeed = bullet.speed * transform.x.x
+			bullet.rotation_degrees = 0
+		elif facing == Enums.Facing.UP:
+			yspeed = -bullet.speed
+			bullet.rotation_degrees = 270
+		elif facing == Enums.Facing.DOWN:
+			yspeed = bullet.speed
+			bullet.rotation_degrees = 90
+		
+		bullet.velocity = Vector2(xspeed, yspeed)
+		get_parent().get_parent().add_child(bullet)
+		
+		state = Enums.State.IDLE
 
 
 func  dead_state():
